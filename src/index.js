01d61679ac7c5ee06441925173d4d066ac7a78c9
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-12 09:09:19 +0900

    fix typo for components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import reducer from './reducers';
-import App from './component/App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer)
